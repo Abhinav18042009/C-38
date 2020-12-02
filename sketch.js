a7b1d4d553cad2fa39fe143f72c82cb2dcd8ec83
Naga Kishore Nori <abnavgaming@gmail.com>
2020-12-02 15:43:51 +0530

    JUuUJuUuU

--- sketch.js
+++ sketch.js
@@ -10,6 +10,14 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+function preload(){
+  pehla = loadImage("images/car1.png");
+  doosra = loadImage("images/car2.png");
+  teesra = loadImage("images/car3.png");
+  chowta = loadImage("images/car4.png");
+  road = loadImage("images/track.jpg");
+  jojo = loadImage("images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
